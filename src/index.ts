c39740fe5c80353b949b67eb47e5ddb2427a3c2a
Mateo Barrios <xd2006@gmail.com>
2022-02-08 10:22:22 -0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Car } from "./components/car";
+
+// Função inicial para startar a aplicação
+const run = () => {
+    console.log("Iniciando as interações entre componentes de um carro");
+    const car1 = new Car();
+}
+
+run();
